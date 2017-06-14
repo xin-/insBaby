56ab7b25bb8a25bb74d56fe165a5f5a1fe4e4f77
xinhonglin <xinhongin@wepiao.com>
2017-06-14 15:11:56 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { PropTypes } from 'react'
+import ReactDOM, { render } from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import todoApp from './reducers'
 import Header from './component/header/index.js'
 import '../css/index.scss'
 
-class Page extends React.Component {
-  render() {
-    return <Header />
-  } 
-}
+let store = createStore(todoApp)
+let rootElement = document.getElementById('app')
 
-ReactDOM.render(<Page />, document.getElementById('app'));
+const myRouter = ({ store }) => 
+		<Router history={browserHistory}>
+	      	<Route path="/" component={Header}/>
+	    </Router>
+
+const Root = ({ store }) => (
+	<Provider store={store}>
+	    <myRouter/>
+	</Provider>
+);
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+};
